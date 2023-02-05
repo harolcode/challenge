3cbfead22ef2641ae27d620bf97065c329052ecb
harolcode <harol.agudelo@coddling.co>
2023-02-04 23:33:32 -0500

    fake data

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,15 +9,8 @@ import { SocialMediaService } from './services/social.media.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  socialsMedia!: SocialMedia[];
-  constructor(private socialMediaSvc: SocialMediaService) {}
+export class DashboardComponent  {
 
-  ngOnInit(): void {
-    this.socialMediaSvc.getSocialsMedia()
-    .pipe(
-      tap((res:SocialMedia[])=> this.socialsMedia = res)
-    ).subscribe();
-  }
+
 
 }
